2e83a172184171f1accd324eb08a576eebcce492
nukappa <nukappa@users.noreply.github.com>
2022-12-05 11:07:41 +0100

    fixed pip installation

--- setup.py
+++ setup.py
@@ -1,30 +1,4 @@
-from setuptools import setup, find_packages
+if __name__ == "__main__":
+    from setuptools import setup
 
-with open("README.rst", "r") as fh:
-    long_description = fh.read()
-
-try:
-    from stimwrap import __author__, __email__, __version__
-except ImportError:  # Deps not yet installed
-    __author__ = __email__ = __version__ = ''
-
-with open('requirements.txt', 'r') as f:
-    required_packages = f.read().splitlines()
-
-setup(
-    name="stimwrap",
-    version=__version__,
-    author=__author__,
-    author_email=__email__,
-    description="stimwrap: a Python interface for STIM.",
-    long_description=long_description,
-    url="https://github.com/nukappa/stimwrap",
-    license='MIT',
-    install_requires=required_packages,
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: POSIX :: Linux",
-        "Operating System :: MacOS :: MacOS X"
-    ]
-)
+    setup()
