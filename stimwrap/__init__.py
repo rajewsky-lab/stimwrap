da642891f7de9a346329f335ffcf0aee91aa45c3
Daniel León-Perinán <daniel.leonperinan@mdc-berlin.de>
2024-07-09 15:33:06 +0200

    reorganize _stimwrap to Container class

--- stimwrap/__init__.py
+++ stimwrap/__init__.py
@@ -1,11 +1,4 @@
-from ._stimwrap import get_container, \
-    get_dataset_names, \
-    get_dataset, \
-    get_attribute_from_dataset, \
-    get_item_from_dataset, \
-    get_gene_expression_from_dataset, \
-    get_transform_matrix, \
-    get_aligned_locations
+from ._stimwrap import Container, Dataset
 
 from ._stim_functions import add_slice, \
     resave, normalize, add_annotations, \
